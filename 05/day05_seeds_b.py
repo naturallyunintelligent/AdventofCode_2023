409a4038cd8b306d1c95884f35fd31b0d3a8238b
Darren Steele <darren.steele@five.ai>
2023-12-29 10:16:28 +0000

    day 5 part b - add some prints to target optimisation

--- 05/day05_seeds_b.py
+++ 05/day05_seeds_b.py
@@ -7,13 +7,16 @@ from re import sub
 input_text_file = "input.txt"
 
 def load_data(input_text_file):
+    print("load_data...")
     with open(input_text_file) as input_file:
         data = []
         for index, line in enumerate(input_file):
             data.append(line.strip())
+    print("...load_data")
     return data
 
 def find_next_seed(data):
+    print("find_next_seed...")
     seeds = []
     range_values = data[0].split()[1:]
     for a, range_value in enumerate(range_values):
@@ -23,13 +26,16 @@ def find_next_seed(data):
         if i % 2 == 0:
             for seed_value in range(range_values[i], range_values[i]+range_values[i+1]):
                 #seeds.append(seed_value)
+                print(f"...find_next_seed {seed_value}")
                 yield seed_value
         else:
             continue
+    print("...find_next_seed False")
     yield False
 
 
 def parse_almanac_to_maps(data):
+    print("parse_almanac_to_maps...")
     maps = {}
     current_map_name = ''
     for line in data:
@@ -42,22 +48,29 @@ def parse_almanac_to_maps(data):
             current_map_name = line[:-1]
         else:
             maps[current_map_name].append(line)
+    print(f"...parse_almanac_to_maps {maps}")
     return maps
 
 def lookup_map_final_value(initial_value, map_values):
+    print("lookup_map_final_value...")
     for row in map_values:
         row_values = row.split()
         if initial_value in range(int(row_values[1]), int(row_values[1]) + int(row_values[2])):
             final_value = int(row_values[0]) + (initial_value - int(row_values[1]))
             return final_value
     final_value = initial_value
+    print(f"...lookup_map_final_value {final_value}")
     return final_value
 
 def extract_name_B(key):
+    print("extract_name_B...")
     name = key.split('-')
+    print("...extract_name_B")
     return name[2].split()[0]
 
 def follow_map(A, initial_value, maps):
+    print("follow_map...")
+
     B = ''
 
     for map in maps:
@@ -69,7 +82,7 @@ def follow_map(A, initial_value, maps):
         else:
             final_value = 'huh?'
             B = 'errrrr'
-
+    print(f"...follow_map {B}, {final_value}")
     return B, final_value
 
 
